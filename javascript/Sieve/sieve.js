adb190d9aaa4c611b8ef736a66ac7c4b5286e61c
Kimberly Wilkes <kimowilkes@gmail.com>
2024-05-31 16:17:33 -0400

    wip: works quickly up to index 2000

--- javascript/Sieve/sieve.js
+++ javascript/Sieve/sieve.js
@@ -1,6 +1,29 @@
 class Sieve {
+  isPrime(num) {
+    if (num % 2 == 0 && num > 2) return false; // even numbers aren't prime
+
+    for (let i = 3; i < num; i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
   NthPrime(n) {
-    throw new Error("unimplemented");
+    const arrayOfNumbers = [2];
+    // const arrayOfPrimes = new Array(n).fill(false);
+    const limit1 = Math.floor(Math.sqrt(n + 1));
+    const limit = n * limit1;
+    for (let i = 3; i < limit; i += 2) {
+      if (this.isPrime(i)) {
+        arrayOfNumbers.push(i);
+        if (arrayOfNumbers.length > n) {
+          console.log("hit length breaking ", arrayOfNumbers.length);
+          break;
+        }
+      }
+    }
+
+    // for (let i = 0; i < limit; i++) {}
+    return arrayOfNumbers[n];
   }
 }
 
